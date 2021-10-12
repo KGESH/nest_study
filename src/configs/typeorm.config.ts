8711b7cc8c4ec7804c321074bc72691a1bad4f0f
KGESH <baram789456@gmail.com>
2021-10-12 17:47:57 +0900

    fix default config

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -2,13 +2,13 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as config from 'config';
 
 const dbConfig: any = config.get('db');
-
+console.log(dbConfig);
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
   host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
-  password: dbConfig.password,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
   database: process.env.RDS_DATABASE || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js, ts}'],
   synchronize: process.env.RDS_SYNCHRONIZE || dbConfig.synchronize,
